1e633912fa1f012e47b6b8e40c22de3d5ddc7cda
LucaKolibius <luca.kolibius@gmail.com>
2020-05-27 11:27:02 +0100

    SFC padding and case for no significant SFC
    I implemented padding for the SFC to increase the frequency resolution
    If no significant SFC is found, I take the biggest eigenvector of the unfiltered approach
    Settings are now all at the beginning of the script
    There is no need for a separate server script

--- indxBase.m
+++ indxBase.m
@@ -6,7 +6,6 @@ addpath(genpath('X:\Luca\functions\wave_clus-master')); % waveclus 3.0
 addpath(genpath('X:\Luca\functions')); % my functions
 addpath('X:\Luca\functions\Neuralynx_19012019'); % Neuralynx (the commons folder function doesnt work on my PC)
 addpath(genpath('X:\Luca\TREBER\Scripts'))
-addpath('Z:\Common\fieldtrip-20170618');
 addpath('X:\Common\fieldtrip-20200310');
 ft_defaults;
 dbstop if error
