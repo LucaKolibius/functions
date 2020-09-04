7f0296166cac0851b1ee3f53adcdfa5fd585ea84
Luca Kolibius <luca.kolibius@gmail.com>
2020-09-04 17:27:19 +0100

    Added orthogolanization to calcRppl

--- calcRppl.m
+++ calcRppl.m
@@ -1,4 +1,4 @@
-function [rpplRec, rpplsWire, bndLab] = calcRppl (data, bidsID, sesh, artFold)
+function [rpplRec, rpplsWire, bndLab] = calcRppl (data, bidsID, sesh, artFold, ortho)
 
 chanLen   =  length( data.time{1} ) ./ 1000;  % timepoint length divided by sampling freq 1024 Hz gives channellength in seconds
 rppl_exp  =  chanLen * (0.023 + 0.00545);           % number of ripples in white noise of same length
@@ -9,6 +9,11 @@ load([artFold, filesep, bidsID, '_', sesh, '_micro_th-8.mat'], 'del_sampleinfo',
 % NAMES OF THE BUNDLES
 bndLab = unique(cellfun(@(x) x(1:end-1),  data.label, 'un', 0));
 
+% ORTHOGONALIZE 
+if ortho == 1
+    [data, success] = orthogonVec(data);
+end
+
 rpplsWire = cell.empty(size(bndLab,1), 0);
 for bund = 1 : size(bndLab,1)
     curBund = cellfun(@(x) regexp(x,bndLab(bund)), {data.label}, 'un', 0);
@@ -16,11 +21,11 @@ for bund = 1 : size(bndLab,1)
     curBund = cellfun(@(x) find(x == 1), curBund, 'un', 0);
     curBund = ~cellfun(@isempty, curBund);
     curBund = find(curBund == 1);
-    
+        
     
     % rpplWire = zeros(sum(selChan),chanLen*1000);
     % rpplVisu = zeros(sum(selChan),chanLen*1000);
-%     rpplsWire{bund,1} = cell.empty(8,0);
+    % rpplsWire{bund,1} = cell.empty(8,0);
     for mw = 1 : size(curBund,1) % LOOP OVER CHANNEL
         
         cfg             = [];
@@ -46,23 +51,23 @@ for bund = 1 : size(bndLab,1)
             rppls = [outRipple.evtIndiv.staTime(~rejects)', outRipple.evtIndiv.endTime(~rejects)'];
             
             rpplsWire{bund,1}{mw,:} = rppls;
-                    for rp = 1:size(rppls,1)
-%                         rpplWire(mw, rppls(rp,1) : rppls(rp,2)) = 1;
-                        rpplVisu(mw, rppls(rp,1) : rppls(rp,1) + 3000) = 1;
-                    end
+            for rp = 1:size(rppls,1)
+                rpplWire(mw, rppls(rp,1) : rppls(rp,2)) = 1;
+%                 rpplVisu(mw, rppls(rp,1) : rppls(rp,1) + 3000) = 1;
+            end
         else
             rpplsWire{bund}{mw,:} = [];
         end
     end
     
-    figure('units','normalized','outerposition',[0 0 1 1])
-    imagesc(rpplVisu)
-    drawnow
+%     figure('units','normalized','outerposition',[0 0 1 1])
+%     imagesc(rpplVisu)
+%     drawnow
     
     % rpplRec = logical(sum(rpplWire,1));
-    rpplRec = [];
+    rpplRec = []; % no longer in use (this was taking all the ripples on the bundle into account; independent on mw)
 end 
 end % end of function
-sub-0002
-662 900
-S1
+% sub-0002
+% 662 900
+% S1
