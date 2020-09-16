7af566ea522dcc28413a47cfad3d14e1d8303224
Luca Kolibius <luca.kolibius@gmail.com>
2020-09-16 22:57:16 +0100

    Fixing perm_ranksum
    When going back to the initial commit (before the reverting) my work on perm_ranksum was also reverted (so perm_ranksum_old was called perm_ranksum again). Luckily I saved the work on perm_ranksum and the name change to the inital perm_ranksum to perm_ranksum_old in a separate folder so I can now simply commit them. I think I have solved this mess now.

--- perm_ranksum.m
+++ perm_ranksum.m
@@ -1,37 +1,23 @@
 function p = perm_ranksum(vec1, vec2)
+
 nperm   = 10000;
 numVec1 = size(vec1,1);
 numVec2 = size(vec2,1);
 
-%% EMPIRICAL RANKSUM
-vec1 = [vec1, ones(size(vec1,1),1)*1]; 
-vec2 = [vec2, ones(size(vec2,1),1)*2]; 
+empDiff = median(vec1) - median(vec2);
 
-empDat = [vec1; vec2];
-
-empDat(:,1) = floor(tiedrank([empDat(:,1)]));
-
-rank1 = sum(empDat(empDat(:,2) == 1,1)) / numVec1;
-rank2 = sum(empDat(empDat(:,2) == 2,1)) / numVec2;
-
-relRank = rank1/rank2;
-
-%% PERMUTATION TEST
-relRankperm = zeros(1,nperm);
+allDat = [vec1; vec2];
 for perm = 1 : nperm
-permDx  = randperm(numVec1 + numVec2);
-permDat = [empDat(permDx,1), empDat(:,2)];
-
-permDat(:,1) = floor(tiedrank([permDat(:,1)]));
-
-permRank1 = sum(permDat(permDat(:,2) == 1,1)) / numVec1;
-permRank2 = sum(permDat(permDat(:,2) == 2,1)) / numVec2;
-
-relRankperm(perm) = permRank1 / permRank2;
-
+    randIdx = randperm(length(allDat));
+    allDat = allDat(randIdx);
+    
+    v1perm = allDat(1:numVec1);
+    v2perm = allDat(numVec1+1:end);
+    
+    permDiff(perm) = median(vec1) - median(vec2);
 end
 
-%% ESTIMATE P-VALUE
-p = 1 - (sum(relRank >= relRankperm) / nperm);
+% p = 1 - mean(empDiff >= permDiff) ; % I think that would work just fine
+p = 1 - (sum(empDiff >= permDiff) / nperm);
 
-end  % END OF FUNCTION
+end
