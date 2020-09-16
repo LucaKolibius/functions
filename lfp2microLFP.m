4420c358ffe0647cd02b953d887f7d9687f34477
Luca Kolibius <luca.kolibius@gmail.com>
2020-09-16 22:27:25 +0100

    lfp2microLFP (loads in Georges LFP and outputs to mine)
    Changed lfp2microLFP to work with only the newest non- pike interpolated 1khz micro LFP (instead of cycling through all LFPs)

--- lfp2microLFP.m
+++ lfp2microLFP.m
@@ -1,56 +1,62 @@
-% various settings
-LFPfold = 'X:\George\Analysis\Data Continuous\no-SPK-interpolation';                     % LFP;
-LFPdir  = dir('X:\George\Analysis\Data Continuous\sub-*.mat.mat');
-artDir = 'Z:\hanslmas-ieeg-compute\George\Analysis\Artefact Rejection\Data Continuous 1000Hz\';    % ARTEFACTS
-
-% start with each SU
-for it = 1:size(LFPdir,1)
-    clearvars -except it LFPfold LFPdir artDir
-    disp(it)
-       
-    % get current bidsID & sesh
-    bidsID = LFPdir(it).name; sesh = bidsID;
-    bidsID = bidsID(1:8);
-    sesh   = sesh(10:11);
-        
-    load([artDir, bidsID, '_', sesh, '_micro_th-8.mat'], 'delIx', 'del_sampleinfo'); % load artefact timestamps (1x8 cell with each cell corresponding to a wire)
-    load([LFPfold, bidsID, '_', sesh, '_micro_RAW_DS-1000_SPK-INT.mat'], 'data_micro') 
-    
-    labs    = data_micro.label; % all labels
-    noSpks  = cellfun(@(x) regexp(x, 'SPKS'), labs, 'un', 0); % all labels apart from the spikes
-    noTrig  = cellfun(@(x) regexp(x, 'TRIG'), labs, 'un', 0); % indexes the trigger
-    noHits  = cellfun(@(x) regexp(x, 'enc+'), labs, 'un', 0); % indexes the hits / miss info
-    selChan = cellfun(@isempty, noSpks);
-    noTrig  = ~cellfun(@isempty, noTrig);
-    noHits  = ~cellfun(@isempty, noHits);
-    
-    % index all the names of LFPs (ignores spikes / trigger / hits and misses)
-    selChan      = logical(selChan - noTrig - noHits);   
-    cfg          = [];
-    cfg.channel  = data_micro.label(selChan);       % all 8 channels
-    data         = ft_selectdata(cfg, data_micro);  % select
-    
-    
-    for chan = 1 : size(del_sampleinfo,2)
-        artChan = del_sampleinfo{chan};
-        for art = 1 : size(artChan,1)
-            data.trial{1}(chan, artChan(art,1) : artChan(art,2) ) = nan;
-        end
-    end
-    
-    save(['X:\Luca\data\microLFP\', bidsID, '_', sesh, '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data')
-    
-end
+% % various settings
+% LFPfold = 'X:\George\Analysis\Data Continuous\no-SPK-interpolation';                     % LFP;
+% LFPdir  = dir('X:\George\Analysis\Data Continuous\sub-*.mat.mat');
+% artDir = 'Z:\hanslmas-ieeg-compute\George\Analysis\Artefact Rejection\Data Continuous 1000Hz\';    % ARTEFACTS
+% 
+% % start with each SU
+% for it = 1:size(LFPdir,1)
+%     clearvars -except it LFPfold LFPdir artDir
+%     disp(it)
+%        
+%     % get current bidsID & sesh
+%     bidsID = LFPdir(it).name; sesh = bidsID;
+%     bidsID = bidsID(1:8);
+%     sesh   = sesh(10:11);
+%         
+%     load([artDir, bidsID, '_', sesh, '_micro_th-8.mat'], 'delIx', 'del_sampleinfo'); % load artefact timestamps (1x8 cell with each cell corresponding to a wire)
+%     load([LFPfold, bidsID, '_', sesh, '_micro_RAW_DS-1000_SPK-INT.mat'], 'data_micro') 
+%     
+%     labs    = data_micro.label; % all labels
+%     noSpks  = cellfun(@(x) regexp(x, 'SPKS'), labs, 'un', 0); % all labels apart from the spikes
+%     noTrig  = cellfun(@(x) regexp(x, 'TRIG'), labs, 'un', 0); % indexes the trigger
+%     noHits  = cellfun(@(x) regexp(x, 'enc+'), labs, 'un', 0); % indexes the hits / miss info
+%     selChan = cellfun(@isempty, noSpks);
+%     noTrig  = ~cellfun(@isempty, noTrig);
+%     noHits  = ~cellfun(@isempty, noHits);
+%     
+%     % index all the names of LFPs (ignores spikes / trigger / hits and misses)
+%     selChan      = logical(selChan - noTrig - noHits);   
+%     cfg          = [];
+%     cfg.channel  = data_micro.label(selChan);       % all 8 channels
+%     data         = ft_selectdata(cfg, data_micro);  % select
+%     
+%     
+%     for chan = 1 : size(del_sampleinfo,2)
+%         artChan = del_sampleinfo{chan};
+%         for art = 1 : size(artChan,1)
+%             data.trial{1}(chan, artChan(art,1) : artChan(art,2) ) = nan;
+%         end
+%     end
+%     
+%     save(['X:\Luca\data\microLFP\', bidsID, '_', sesh, '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data')
+%     
+% end
 
 %% FROM LAB PC
 % various settings
 % CHANGED FOR NO SPK INTERPOLATION!!
+
+clear
 addpath('X:\Common\toolboxes\fieldtrip-20200310');
 ft_defaults;
-LFPfold = 'X:\George\Analysis\Data Continuous\';                     % LFP;
-LFPdir  = dir('X:\George\Analysis\Data Continuous\sub-*MACRO_RAW.mat');
+LFPfold = 'X:\George\Analysis\Data Continuous\no-SPK-interpolation\';   % LFP;
+LFPdir  = dir([LFPfold, 'sub-*_micro_RAW_DS-1000_SPK-INT.mat']);
 % artDir = 'X:\George\Analysis\Artefact Rejection\Data Continuous 1000Hz\';    % ARTEFACTS
 
+
+%% ONLY SELECT NEW DATA
+newLFPidx = cell2mat(cellfun(@(x) contains(x, '16-Sep-2020'), {LFPdir.date}, 'un', 0));
+LFPdir = LFPdir(newLFPidx);
 % start with each SU
 for it = 1:size(LFPdir,1)
     clearvars -except it LFPfold LFPdir artDir
@@ -62,29 +68,33 @@ for it = 1:size(LFPdir,1)
     sesh   = sesh(10:11);
         
 %     load([artDir, bidsID, '_', sesh, '_micro_th-8.mat'], 'delIx', 'del_sampleinfo'); % load artefact timestamps (1x8 cell with each cell corresponding to a wire)
-%     load([LFPfold, bidsID, '_', sesh, '_micro_RAW_DS-1000_SPK-INT.mat'], 'data_micro') 
-    load([LFPdir(it).folder, filesep, LFPdir(it).name], 'data_macro')
+
+%% MICRO
+load([LFPfold, bidsID, '_', sesh, '_micro_RAW_DS-1000_SPK-INT.mat'], 'data_micro') 
+
+%% MACRO
+%     load([LFPdir(it).folder, filesep, LFPdir(it).name], 'data_macro')
     
-%     %% FOR MICROS
-%     labs    = data_macro.label; % all labels
-%     noSpks  = cellfun(@(x) regexp(x, 'SPKS'), labs, 'un', 0); % all labels apart from the spikes
-%     noTrig  = cellfun(@(x) regexp(x, 'TRIG'), labs, 'un', 0); % indexes the trigger
-%     noHits  = cellfun(@(x) regexp(x, 'enc+'), labs, 'un', 0); % indexes the hits / miss info
-%     selChan = cellfun(@isempty, noSpks);
-%     noTrig  = ~cellfun(@isempty, noTrig);
-%     noHits  = ~cellfun(@isempty, noHits);
-%     
+    %% FOR MICROS
+    labs    = data_micro.label; % all labels
+    noSpks  = cellfun(@(x) regexp(x, 'SPKS'), labs, 'un', 0); % all labels apart from the spikes
+    noTrig  = cellfun(@(x) regexp(x, 'TRIG'), labs, 'un', 0); % indexes the trigger
+    noHits  = cellfun(@(x) regexp(x, 'enc'), labs, 'un', 0); % indexes the hits / miss info
+    selChan = cellfun(@isempty, noSpks);
+    noTrig  = ~cellfun(@isempty, noTrig);
+    noHits  = ~cellfun(@isempty, noHits);
+    
 %     % index all the names of LFPs (ignores spikes / trigger / hits and misses)
-%     selChan      = logical(selChan - noTrig - noHits);   
+    selChan      = logical(selChan - noTrig - noHits);   
     
     %% FOR MACROS
-    labs = cellfun(@(x) x(end), data_macro.label, 'un', 0);
-    labs = str2double(labs);    
-    selChan = labs == 1;
+%     labs = cellfun(@(x) x(end), data_macro.label, 'un', 0);
+%     labs = str2double(labs);    
+%     selChan = labs == 1;
     
     cfg          = [];
-    cfg.channel  = data_macro.label(selChan);       % all 8 channels
-    data         = ft_selectdata(cfg, data_macro);  % select
+    cfg.channel  = data_micro.label(selChan);       % all 8 channels
+    data         = ft_selectdata(cfg, data_micro);  % select
     
     
 %     for chan = 1 : size(del_sampleinfo,2)
