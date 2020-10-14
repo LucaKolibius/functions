b32dc069af8d46491891698e24be779b891768f0
Luca Kolibius <luca.kolibius@gmail.com>
2020-10-14 13:56:18 +0100

    Updated path for spkRppl_ppc

--- spkRppl_ppc.m
+++ spkRppl_ppc.m
@@ -1,8 +1,8 @@
 %% SPIKE PPC FOR RIPPLE FREQUENCY BAND (spike ripple ppc)
 %  Now adapted to run in a parfor loop
 %  This does not take any artefacts into account (they have to be NaN'ed before 
-
-server = 0;
+clear
+server = 1;
 
 switch server
     % RUN ON MY OWN LAPTOP
@@ -10,20 +10,20 @@ switch server
         addpath('X:\Common\toolboxes\fieldtrip-20200310');
         ft_defaults
         allMicro = 'X:\Luca\data\microLFP\';
-        load('X:\Luca\data\allSbj\allSpks.mat', 'allSpks');
+        load('X:\Luca\data\allSbj\allSpksHZ.mat', 'allSpks');
         
     % RUN ON THE SERVER
     case 1
         addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
         ft_defaults
         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/';
-        load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/fileexchange covid/allSpks.mat', 'allSpks');
+        load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
 end
 
 parfor it = 1 : size(allSpks,2)
-    tic
+    disp(it)
     
-    spks    = allSpks(it).spks;
+    spks = allSpks(it).spks;
     spks = round(spks);
     
     if size(spks,1) > 1000
@@ -40,7 +40,7 @@ parfor it = 1 : size(allSpks,2)
     bundlename = allSpks(it).bundlename;
     
     % LOAD LFP
-    data = load([allMicro, bidsID, '_', sesh, '_onlyMicroLFP_RAW_1000DS_SPKINT.mat'], 'data');
+    data = load([allMicro, bidsID, '_', sesh, '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data');
     data = data.data;
 
     % SELECT BUNDLE LFP
@@ -52,13 +52,15 @@ parfor it = 1 : size(allSpks,2)
     numChan      = size(data.label,1);
 
     % FREQUENCY ANALYSIS TO GET THE PHASE VALUES
+    tic
     cfgtf        = [];
     cfgtf.method = 'wavelet';
     cfgtf.width  = 8;
-    cfgtf.toi    = 1:100; %'all';
+    cfgtf.toi    = 'all';
     cfgtf.foi    = logspace(log10(1),log10(140))
     cfgtf.output = 'fourier';
     fspec        = ft_freqanalysis(cfgtf,data);
+    toc
     
     phs          = fspec.fourierspctrm;
     phs          = angle(phs);   % trl=1 / chan / foi / toi
@@ -82,7 +84,9 @@ parfor it = 1 : size(allSpks,2)
             
             % LOOP OVER SPIKES
             for curSpk = 1 : size(spks,1) - 1
+%                 updater = fprintf(1,'SU: %d/%d | channel: %d/%d | freq: %d/%d | spike: %d/%d | timer: %d \n', it, size(allSpks,2), chan, numChan, curFreq, size(cfgtf.foi,2), curSpk, size(spks,1)-1, toc);
                 spkDum(curSpk) = nansum( cos( chanFoiPhs ( spks(curSpk)) - chanFoiPhs ( spks(curSpk+1 : end)) ) );
+%                 fprintf(repmat('\b', 1, updater));
             end
             freqDum(curFreq) = nansum(spkDum) / (numSpks*(numSpks-1)/2);
             
@@ -92,11 +96,11 @@ parfor it = 1 : size(allSpks,2)
     
     allSpks(it).ppc = chanFreqDum; %% SAVE OUTPUT IN MY STRUCTURE
     
-    doneSecs = toc;
-    fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
-    fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
-    fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
-    fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
+%     doneSecs = toc;
+%     fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
+%     fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
+%     fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
+%     fprintf('Spike number %d. Took %.2f seconds.\n', it, doneSecs)
   
 end
 save('spkRpplPPC', 'allSpks');
