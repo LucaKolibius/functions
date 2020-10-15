241b1b4b9f6b7ebcc10a28594efcf0c40aa20e41
Luca Kolibius <luca.kolibius@gmail.com>
2020-10-15 19:04:56 +0100

    dmeanOrthLFP has phase info now
    Variable too large, so added -v7.3

--- dmeanOrthLFP.m
+++ dmeanOrthLFP.m
@@ -2,12 +2,21 @@
 
 function dmeanOrthLFP
 
-load('X:\Luca\data\allSbj\allSpksHZ.mat', 'allSpks');
-lfpDir     = dir('X:\Luca\data\microLFP\sub-*_onlyMicroLFP_RAW_1000DS_noSPKINT.mat');  % NO SPK-INT
-savefolder = 'X:\Luca\data\microLFP_dmeanOrth\';
+% %% LAB PC
+% load('X:\Luca\data\allSbj\allSpksHZ.mat', 'allSpks');
+% lfpDir     = dir('X:\Luca\data\microLFP\sub-*_onlyMicroLFP_RAW_1000DS_noSPKINT.mat');  % NO SPK-INT
+% savefolder = 'X:\Luca\data\microLFP_dmeanOrth\';
+
+
+%% CASTLES VM
+addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
+ft_defaults
+
+load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
+savefolder = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP_dmeanOrth/';
 
 for spk = 1 :  size(allSpks,2)
-    
+    disp(spk);
     
     %% SKIP REPEATING MICROWIRE BUNDLES
     if spk > 1
@@ -23,8 +32,9 @@ for spk = 1 :  size(allSpks,2)
     allBund = {allSpks.bundlename};
     
     %% LOAD LFP
-    load([lfpDir(1).folder, filesep, bidsID, '_', regexprep(sesh, 'S1b', 'S1'), '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data');
-    
+    load(['/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/', bidsID, '_', regexprep(sesh, 'S1b', 'S1'), '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data');
+%     load(['X:/Luca/data/microLFP/', bidsID, '_', regexprep(sesh, 'S1b', 'S1'), '_onlyMicroLFP_RAW_1000DS_noSPKINT.mat'], 'data');
+
     %% DEMEAN
     cfg        = [];
     cfg.demean = 'yes';
@@ -42,8 +52,14 @@ for spk = 1 :  size(allSpks,2)
     cfgtf.output = 'fourier';
     fspec        = ft_freqanalysis(cfgtf,data);   
     
+    phsAn.phs    = squeeze(angle(fspec.fourierspctrm)); % trl(=1) x chan x freq x time
+    phsAn.label  = fspec.label;
+    phsAn.time   = fspec.time;
+    phsAn.freq   = fspec.freq;
+    
+    
     %% SAVE LFP
-    save([savefolder, bidsID, '_', regexprep(sesh, 'S1b', 'S1'), '_onlyMicroLFP_dmeanOrth_1000DS_noSPKINT.mat'], 'data', 'fspec');
+    save([savefolder, bidsID, '_', regexprep(sesh, 'S1b', 'S1'), '_onlyMicroLFP_dmeanOrth_1000DS_noSPKINT.mat'], 'data', 'phs', '-v7.3');
     
 end % END OF SPK FOR LOOP
 end % END OF FUNCTION
