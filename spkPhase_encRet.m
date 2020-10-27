2ca5eae4bf6f907105d82074225ac471be17331f
Luca Kolibius <luca.kolibius@gmail.com>
2020-10-27 19:14:52 +0000

    spkPhs_encRet takes all spikes into account, max zscore over freq & chan
    The zscore is calculated using a Rayleigh-Test

--- spkPhase_encRet.m
+++ spkPhase_encRet.m
@@ -1,11 +1,10 @@
 %% SPIKE PPC 
 %  This does not take any artefacts into account (they have to be NaN'ed before
 clear
-server = 1;
+server = 0;
 
 switch server
-    % RUN ON MY OWN LAPTOP
-    case 0
+    case 0     % RUN ON MY OWN LAPTOP
         addpath('X:\Common\toolboxes\fieldtrip-20200310');
         addpath('X:\Luca\toolboxes\CircStat2012a');
         ft_defaults
@@ -13,14 +12,14 @@ switch server
         allMicro = 'X:\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
         load('X:\Luca\data\allSbj\allSpksHZ.mat', 'allSpks');
         
-        % RUN ON THE SERVER
-        %     case 1
-        %         addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
-        %         ft_defaults
-        %         %         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/';
-        %         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
+            case 1         % RUN ON THE SERVER
+
+                addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
+                ft_defaults
+                %         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/';
+                allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
         
-        %         load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
+                load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
 end
 
 prevLFPname = [];
@@ -46,22 +45,30 @@ for su = 1 : size(allSpks,2)
     wirename   = allSpks(su).wirename;
     bundlename = allSpks(su).bundlename;
     
-    lfpName = [allMicro, bidsID, '_', sesh, '_onlyMicroLFP_dmeanOrth_1000DS_noSPKINT.mat'];
+    lfpName = [allMicro, bidsID, '_', sesh, '_', bundlename, '_onlyMicroLFP_dmeanOrth_1000DS_noSPKINT.mat'];
     
     %% ONLY LOAD IN LFP IF IT IS A NEW ONE
     if ~strcmp(lfpName, prevLFPname)
-        lfp = load(lfpName, 'phsAn');
-        lfp = lfp.data;
+        loaded = 0;
+        while loaded == 0
+            try
+                load(lfpName, 'phsAn');
+                loaded = 1;
+            catch
+                pause(60)
+            end
+        end
+        
         prevLFPname = lfpName;
     end
     
-    %%% SELECT BUNDLE LFP
-    selChan      = contains(lfp.label, bundlename);
-    cfg          = [];
-    cfg.channel  = lfp.label(selChan);       % all 8 channels
-    bundLFP      = ft_selectdata(cfg, lfp);  % select
-    
-    numChan      = size(bundLFP.label,1);
+%     %% SELECT BUNDLE LFP
+%     selChan      = contains(lfp.label, bundlename);
+%     cfg          = [];
+%     cfg.channel  = lfp.label(selChan);       % all 8 channels
+%     bundLFP      = ft_selectdata(cfg, lfp);  % select
+%     
+%     numChan      = size(bundLFP.label,1);
     
     %% WHICH TRIALS DOES THIS SU INDEX?
     idxTrl = allSpks(su).idxTrl;
@@ -73,43 +80,52 @@ for su = 1 : size(allSpks,2)
     encTrig  = round(allSpks(su).encTrigger(allSpks(su).hitsIdx,[1 3])*1000);
     retTrig  = round(allSpks(su).retTrigger(allSpks(su).hitsIdx,[1 3])*1000);
     
-    %% REDEFINE TRIALS ACCORDING TO encTrig
-    cfg            = [ ];
-    cfg.trl        = [encTrig(:,1) encTrig(:,2) zeros(size(encTrig,1),1)];
-    bundLFPencTrl  = ft_redefinetrial(cfg, bundLFP);
-    
-    %% REDEFINE TRIALS ACCORDING TO retTrig
-    cfg            = [ ];
-    cfg.trl        = [retTrig(:,1) retTrig(:,2) zeros(size(retTrig,1),1)];
-    bundLFPretTrl  = ft_redefinetrial(cfg, bundLFP);
+%     %% REDEFINE TRIALS ACCORDING TO encTrig
+%     cfg            = [ ];
+%     cfg.trl        = [encTrig(:,1) encTrig(:,2) zeros(size(encTrig,1),1)];
+%     bundPHSencTrl  = ft_redefinetrial(cfg, phsAn);
+%     
+%     %% REDEFINE TRIALS ACCORDING TO retTrig
+%     cfg            = [ ];
+%     cfg.trl        = [retTrig(:,1) retTrig(:,2) zeros(size(retTrig,1),1)];
+%     bundPHSretTrl  = ft_redefinetrial(cfg, phsAn);
     
     %% LOOP OVER TRIALS
     for trl = 1 : size(encTrig,1)        
         % SPIKES THAT OCCUR WITHIN THIS TRIAL DURING ENCODING
-        encSpkTrl = spks>=encTrig(1,trl) && spks <= encTrig(2,trl);
-        if ~isempty(encSpkTrl); encSpkTrl = encSpkTrl(1); end % take the first spike
+        encSpkTrl = spks(spks>=encTrig(trl, 1) & spks <= encTrig(trl, 2)); % ALL SPIKES THAT OCCUR WITHIN THE CURRENT TRIAL
+%         if ~isempty(encSpkTrl); encSpkTrl = encSpkTrl(1); end % take the
+%         first spike (outdated)
         
         % SPIKES THAT OCCUR WITHIN THIS TRIAL DURING RETRIEVAL
-        retSpkTrl = spks>=retTrig(1,trl) && spks <= retTrig(2,trl);
-        if ~isempty(retSpkTrl); retSpkTrl = retSpkTrl(1); end % take the first spike
+        retSpkTrl = spks(spks>=retTrig(trl, 1) & spks <= retTrig(trl, 2));
+%         if ~isempty(retSpkTrl); retSpkTrl = retSpkTrl(1); end % take the
+%         first spike (outdated)
         
-        allSpks(su).encPhs(:,:,trl) = phsAn.phs(encSpkTrl:::);
-        allSpks(su).retPhs(:,:,trl) = phsAn.phs(retSpkTrl:::);
+        allSpks(su).encPhs(trl,1) = {phsAn.phs(:,:,encSpkTrl)}; % in a cell because variable number of spikes per encoding trial
+        allSpks(su).retPhs(trl,1) = {phsAn.phs(:,:,retSpkTrl)};
     end
     
     %% WHICH CHANNEL IS THE CHOSEN ONE?    
-    chanPhsEnc = resize(allSpks(su.encPhs), 8, [], 1); % CHECK IF CORRECT
-    chanPhsRet = resize(allSpks(su.retPhs), 8, [], 1); % CHECK IF CORRECT
+    chanPhsEnc = cat(3, allSpks(su).encPhs{:});
+    chanPhsRet = cat(3, allSpks(su).retPhs{:});
 
     for chan = 1:8
-        [plvl(chan,1) zlvl(chan,1)] = circ_rtest(chanPhsEnc);
-        [plvl(chan,2) zlvl(chan,2)] = circ_rtest(chanPhsRet);
+        for freq = 1:size(chanPhsEnc,2)
+            [~, zlvlEnc(chan,freq)] = circ_rtest(squeeze(chanPhsEnc(chan, freq,:))); 
+            [~, zlvlRet(chan,freq)] = circ_rtest(squeeze(chanPhsRet(chan, freq,:)));
+        end
     end
     
-    [encRetVal,~] = min(plvl,[],2); % take either minimum of encoding or retrieval
-    [~, chanChos] = min(encRetVal); % take lowest channel
+%     for chan = 1:8
+%         [plvl(chan,1) zlvl(chan,1)] = circ_rtest(chanPhsEnc);
+%         [plvl(chan,2) zlvl(chan,2)] = circ_rtest(chanPhsRet);
+%     end
     
-    allSpks(su).LFPchan = chanChos;
+    encRetMax = max(zlvlEnc, zlvlRet); % take maximum z-value of encoding vs. retrieval
+    chanMax = max(encRetMax, [], 2); % for each channel, take the maximum value over the frequency spectrum
+    [~, chosChan] =max(chanMax);
+    allSpks(su).LFPchan = chosChan;
 
 end
 
