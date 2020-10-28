5223261b4fdec84c9221caf1274ff07e1eaeff32
Luca Kolibius <luca.kolibius@gmail.com>
2020-10-28 11:51:32 +0000

    formatting spkPhs_encRet
    I had to kick out one spike that was an artifact from allSpksHZ. I then re-run the findIndexSU_hz script to find sign #IU. parameters: 99, 1.645, 1.645, 0, 0)

--- spkPhase_encRet.m
+++ spkPhase_encRet.m
@@ -1,4 +1,4 @@
-%% SPIKE PPC 
+%% SPIKE PPC
 %  This does not take any artefacts into account (they have to be NaN'ed before
 clear
 server = 0;
@@ -12,14 +12,14 @@ switch server
         allMicro = 'X:\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
         load('X:\Luca\data\allSbj\allSpksHZ.mat', 'allSpks');
         
-            case 1         % RUN ON THE SERVER
-
-                addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
-                ft_defaults
-                %         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/';
-                allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
+    case 1         % RUN ON THE SERVER
         
-                load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
+        addpath('/castles/nr/projects/h/hanslmas-ieeg-compute/Common/toolboxes/fieldtrip-20200310');
+        ft_defaults
+        %         allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/microLFP/';
+        allMicro = '/castles/nr/projects/h/hanslmas-ieeg-compute\Luca\data\microLFP_dmeanOrth\'; % demeaned and orth. LFP
+        
+        load('/castles/nr/projects/h/hanslmas-ieeg-compute/Luca/data/allSbj/allSpksHZ.mat', 'allSpks');
 end
 
 prevLFPname = [];
@@ -59,13 +59,13 @@ for su = 1 : size(allSpks,2)
         prevLFPname = lfpName;
     end
     
-%     %% SELECT BUNDLE LFP
-%     selChan      = contains(lfp.label, bundlename);
-%     cfg          = [];
-%     cfg.channel  = lfp.label(selChan);       % all 8 channels
-%     bundLFP      = ft_selectdata(cfg, lfp);  % select
-%     
-%     numChan      = size(bundLFP.label,1);
+    %     %% SELECT BUNDLE LFP
+    %     selChan      = contains(lfp.label, bundlename);
+    %     cfg          = [];
+    %     cfg.channel  = lfp.label(selChan);       % all 8 channels
+    %     bundLFP      = ft_selectdata(cfg, lfp);  % select
+    %
+    %     numChan      = size(bundLFP.label,1);
     
     %% WHICH TRIALS DOES THIS SU INDEX?
     idxTrl = allSpks(su).idxTrl;
@@ -77,53 +77,53 @@ for su = 1 : size(allSpks,2)
     encTrig  = round(allSpks(su).encTrigger(allSpks(su).hitsIdx,[1 3])*1000);
     retTrig  = round(allSpks(su).retTrigger(allSpks(su).hitsIdx,[1 3])*1000);
     
-%     %% REDEFINE TRIALS ACCORDING TO encTrig
-%     cfg            = [ ];
-%     cfg.trl        = [encTrig(:,1) encTrig(:,2) zeros(size(encTrig,1),1)];
-%     bundPHSencTrl  = ft_redefinetrial(cfg, phsAn);
-%     
-%     %% REDEFINE TRIALS ACCORDING TO retTrig
-%     cfg            = [ ];
-%     cfg.trl        = [retTrig(:,1) retTrig(:,2) zeros(size(retTrig,1),1)];
-%     bundPHSretTrl  = ft_redefinetrial(cfg, phsAn);
+    %     %% REDEFINE TRIALS ACCORDING TO encTrig
+    %     cfg            = [ ];
+    %     cfg.trl        = [encTrig(:,1) encTrig(:,2) zeros(size(encTrig,1),1)];
+    %     bundPHSencTrl  = ft_redefinetrial(cfg, phsAn);
+    %
+    %     %% REDEFINE TRIALS ACCORDING TO retTrig
+    %     cfg            = [ ];
+    %     cfg.trl        = [retTrig(:,1) retTrig(:,2) zeros(size(retTrig,1),1)];
+    %     bundPHSretTrl  = ft_redefinetrial(cfg, phsAn);
     
     %% LOOP OVER TRIALS
-    for trl = 1 : size(encTrig,1)        
+    for trl = 1 : size(encTrig,1)
         % SPIKES THAT OCCUR WITHIN THIS TRIAL DURING ENCODING
         encSpkTrl = spks(spks>=encTrig(trl, 1) & spks <= encTrig(trl, 2)); % ALL SPIKES THAT OCCUR WITHIN THE CURRENT TRIAL
-%         if ~isempty(encSpkTrl); encSpkTrl = encSpkTrl(1); end % take the
-%         first spike (outdated)
+        %         if ~isempty(encSpkTrl); encSpkTrl = encSpkTrl(1); end % take the
+        %         first spike (outdated)
         
         % SPIKES THAT OCCUR WITHIN THIS TRIAL DURING RETRIEVAL
         retSpkTrl = spks(spks>=retTrig(trl, 1) & spks <= retTrig(trl, 2));
-%         if ~isempty(retSpkTrl); retSpkTrl = retSpkTrl(1); end % take the
-%         first spike (outdated)
+        %         if ~isempty(retSpkTrl); retSpkTrl = retSpkTrl(1); end % take the
+        %         first spike (outdated)
         
         allSpks(su).encPhs(trl,1) = {phsAn.phs(:,:,encSpkTrl)}; % in a cell because variable number of spikes per encoding trial
         allSpks(su).retPhs(trl,1) = {phsAn.phs(:,:,retSpkTrl)};
     end
     
-    %% WHICH CHANNEL IS THE CHOSEN ONE?    
-    chanPhsEnc = cat(3, allSpks(su).encPhs{:});
+    %% WHICH CHANNEL IS THE CHOSEN ONE?
+    chanPhsEnc = cat(3, allSpks(su).encPhs{:}); % this is empty if there was not a single spike in any of the encoding trials
     chanPhsRet = cat(3, allSpks(su).retPhs{:});
-
+    
     for chan = 1:8
         for freq = 1:size(chanPhsEnc,2)
-            [~, zlvlEnc(chan,freq)] = circ_rtest(squeeze(chanPhsEnc(chan, freq,:))); 
+            [~, zlvlEnc(chan,freq)] = circ_rtest(squeeze(chanPhsEnc(chan, freq,:)));
             [~, zlvlRet(chan,freq)] = circ_rtest(squeeze(chanPhsRet(chan, freq,:)));
         end
     end
     
-%     for chan = 1:8
-%         [plvl(chan,1) zlvl(chan,1)] = circ_rtest(chanPhsEnc);
-%         [plvl(chan,2) zlvl(chan,2)] = circ_rtest(chanPhsRet);
-%     end
+    %     for chan = 1:8
+    %         [plvl(chan,1) zlvl(chan,1)] = circ_rtest(chanPhsEnc);
+    %         [plvl(chan,2) zlvl(chan,2)] = circ_rtest(chanPhsRet);
+    %     end
     
     encRetMax = max(zlvlEnc, zlvlRet); % take maximum z-value of encoding vs. retrieval
     chanMax = max(encRetMax, [], 2); % for each channel, take the maximum value over the frequency spectrum
     [~, chosChan] =max(chanMax);
     allSpks(su).LFPchan = chosChan;
-
+    
 end
 
 save('X:\Luca\data\allSbj\allSpksHZ_phs', 'allSpks');
