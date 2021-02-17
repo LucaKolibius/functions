432b0177f904315dc155818fd5f46079577cb5fb
Luca <luca.kolibius@gmail.com>
2021-02-17 16:23:55 +0000

    Included power at spiketimes and ran it for lower gamma (34-70hz)

--- SFC_encRet_idxNdx.m
+++ SFC_encRet_idxNdx.m
@@ -19,11 +19,32 @@ allPhsDat.encNdx = [];
 allPhsDat.encIdx = [];
 allPhsDat.retNdx = [];
 allPhsDat.retIdx = [];
+
+allPowDat.encNdx = [];
+allPowDat.encIdx = [];
+allPowDat.retNdx = [];
+allPowDat.retIdx = [];
+
 for su = 1: size(allSpks,2)
+    
+    allPhsDat(su).encNdx = [];
+    allPhsDat(su).encIdx = [];
+    allPhsDat(su).retNdx = [];
+    allPhsDat(su).retIdx = [];
+    
+    allPowDat(su).encNdx = [];
+    allPowDat(su).encIdx = [];
+    allPowDat(su).retNdx = [];
+    allPowDat(su).retIdx = [];
+    
     if allSpks(su).iu == 0
         continue
     end
     
+    disp(su)
+    disp(su)
+    disp(su)
+    disp(su)
     disp(su)
     
     spks = allSpks(su).spks;
@@ -66,8 +87,8 @@ for su = 1: size(allSpks,2)
     retTrig  = round(allSpks(su).retTrigger(allSpks(su).hitsIdx,[1 3])*1000);
     
     %% EXTENDING TRIALS FOR FREQ-ANAL (to get the phase values without nan's at the sides)
-    encTrigEx = [encTrig(:,1)-5000 encTrig(:,2)+5000];
-    retTrigEx = [retTrig(:,1)-5000 retTrig(:,2)+5000];
+    encTrigEx = [encTrig(:,1)-170 encTrig(:,2)+170];
+    retTrigEx = [retTrig(:,1)-170 retTrig(:,2)+170];
     
     %% REDEFINE TRIALS ACCORDING TO encTrig (for AR)
     cfg        = [ ];
@@ -115,22 +136,24 @@ for su = 1: size(allSpks,2)
         cfgtf.output = 'fourier';
         cfgtf.trials = trl;
         
-        
         %% HIGH
         cfgtf.width   = 12;
-        cfgtf.foi     = 60:2:160;
+        %         cfgtf.foi     = 60:2:160;
+        cfgtf.foi     = 34:2:70;
         fspecEncHigh  = ft_freqanalysis(cfgtf, encLFPex);
         fspecRetHigh  = ft_freqanalysis(cfgtf, retLFPex);
         
         %% HIGH-ENCODING
-        encPhsHigh = squeeze(fspecEncHigh.fourierspctrm); % FOURIER SPECTRUM OF THAT TRIAL
-        encPhsHigh = encPhsHigh(:,5001:end-5000); % SNIP WINGS
-        encPhsHigh = angle(encPhsHigh); % TAKE ANGLE
+        encHigh = squeeze(fspecEncHigh.fourierspctrm); % FOURIER SPECTRUM OF THAT TRIAL
+        encHigh = encHigh(:,171:end-170);              % SNIP WINGS
+        encPowHigh = abs(encHigh);                     % TAKE ABSOLUTE FOR POWER
+        encPhsHigh = angle(encHigh);                   % TAKE ANGLE FOR PHASE
         
         %% HIGH-RETRIEVAL
-        retPhsHigh = squeeze(fspecRetHigh.fourierspctrm); % FOURIER SPECTRUM OF THAT TRIAL
-        retPhsHigh = retPhsHigh(:,5001:end-5000); % SNIP WINGS
-        retPhsHigh = angle(retPhsHigh); % TAKE ANGLE
+        retHigh = squeeze(fspecRetHigh.fourierspctrm); % FOURIER SPECTRUM OF THAT TRIAL
+        retHigh = retHigh(:,171:end-170);              % SNIP WINGS
+        retPowHigh = abs(retHigh);                     % TAKE ABSOLUTE FOR POWER
+        retPhsHigh = angle(retHigh);                   % TAKE ANGLE FOR PHASE
         
         %% SPIKES
         % WITHIN ENCODING TRIAL
@@ -147,11 +170,16 @@ for su = 1: size(allSpks,2)
         encPhsHigh(isAR)              = NaN;
         phsDat                        = encPhsHigh(:,encSpkTrl);
         
+        encPowHigh(isAR)              = NaN;
+        powDat                        = encPowHigh(:,encSpkTrl);
+        
         switch idxTrl(trl)
             case 0
-                allPhsDat(su).encNdx = [allPhsDat.encNdx, phsDat];
+                allPhsDat(su).encNdx = [allPhsDat(su).encNdx, phsDat];
+                allPowDat(su).encNdx = [allPowDat(su).encNdx, powDat];
             case 1
-                allPhsDat(su).encIdx = [allPhsDat.encIdx, phsDat];
+                allPhsDat(su).encIdx = [allPhsDat(su).encIdx, phsDat];
+                allPowDat(su).encIdx = [allPowDat(su).encIdx, powDat];
         end
                    
         % RETRIEVAL
@@ -159,19 +187,25 @@ for su = 1: size(allSpks,2)
         isAR                          = squeeze(isAR(favChan,length(cfgtf.foi),:));
         retPhsHigh(isAR)              = NaN;
         phsDat                        = retPhsHigh(:,retSpkTrl);
-
+        
+        retPowHigh(isAR)              = NaN;
+        powDat                        = retPowHigh(:,retSpkTrl);
+        
         switch idxTrl(trl)
             case 0
-                allPhsDat(su).retNdx = [allPhsDat.retNdx, phsDat];
+                allPhsDat(su).retNdx = [allPhsDat(su).retNdx, phsDat];
+                allPowDat(su).retNdx = [allPowDat(su).retNdx, powDat];
             case 1
-                allPhsDat(su).retIdx = [allPhsDat.retIdx, phsDat];
+                allPhsDat(su).retIdx = [allPhsDat(su).retIdx, phsDat];
+                allPowDat(su).retIdx = [allPowDat(su).retIdx, powDat];
         end        
     end
     
    
 end
 
-save('\\analyse4.psy.gla.ac.uk\project0309\Luca\data\allSbj\allPhsDat.mat', 'allPhsDat');
+% save('\\analyse4.psy.gla.ac.uk\project0309\Luca\data\allSbj\allPhsPowDat.mat', 'allPhsDat', 'allPowDat');
+save('\\analyse4.psy.gla.ac.uk\project0309\Luca\data\allSbj\allPhsPowDat_34hz_70hz.mat', 'allPhsDat', 'allPowDat');
 
 % figure(1); clf;
 % subplot(211); hold on;
